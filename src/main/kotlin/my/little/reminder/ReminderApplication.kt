8bc5a998638145606444732c38a7730e86ba51a9
malchun <zmalchunz@gmail.com>
2021-12-25 21:42:00 +0300

    add propper swagger support

--- src/main/kotlin/my/little/reminder/ReminderApplication.kt
+++ src/main/kotlin/my/little/reminder/ReminderApplication.kt
@@ -1,10 +1,24 @@
 package my.little.reminder
 
 import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 
-fun main(args: Array<String>) {
-    Micronaut.build()
-        .args(*args)
-        .packages("my.little.reminder.tasks")
-        .start()
+@OpenAPIDefinition(
+    info = Info(
+        title = "Simple reminder application",
+        version = "0.0",
+        description = "My API",
+        license = License(name = "Apache 2.0", url = "https://foo.bar")
+    )
+)
+object ReminderApplication {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+            .args(*args)
+            .packages("my.little.reminder.tasks")
+            .start()
+    }
 }
